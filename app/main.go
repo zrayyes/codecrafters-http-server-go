3785b7a37fb9d9d8a4103be001378b0a552805ad
Zuhair Rayyes <zrayyes@itgsoftware.com>
2025-12-23 13:13:19 +0200

    Add response struct

--- app/main.go
+++ app/main.go
@@ -6,10 +6,37 @@ import (
 	"os"
 )
 
+// The first line of a Response message is the Status-Line,
+// consisting of the protocol version followed by a numeric status code and its associated textual phrase,
+// with each element separated by SP characters.
+// No CR or LF is allowed except in the final CRLF sequence.
+// Status-Line = HTTP-Version SP Status-Code SP Reason-Phrase CRLF
+type StatusLine struct {
+	HTTPVersion  string
+	StatusCode   int
+	ReasonPhrase string
+}
+
+type Response struct {
+	StatusLine
+}
+
+func (r Response) String() string {
+	return fmt.Sprintf("%s %d %s\r\n\r\n", r.HTTPVersion, r.StatusCode, r.ReasonPhrase)
+}
+
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
-	response := "HTTP/1.1 200 OK\r\n\r\n"
-	_, err := conn.Write([]byte(response))
+
+	r := Response{
+		StatusLine: StatusLine{
+			HTTPVersion:  "HTTP/1.1",
+			StatusCode:   200,
+			ReasonPhrase: "OK",
+		},
+	}
+
+	_, err := conn.Write([]byte(r.String()))
 	if err != nil {
 		fmt.Println("Error writing to connection: ", err.Error())
 		return
